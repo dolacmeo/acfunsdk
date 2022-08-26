ad6e273d6996fbdd687f4aa840d20dd140212437
dolacmeo <dolacmeo@qq.com>
2022-08-26 16:51:43 +0800

    add websocket

---
+++ acfun/page/websocket.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+__author__ = 'dolacmeo'
+__project__ = ''
+__doc__ = ''
+
+# https://ali-imgs.acfun.cn/kos/nlav10360/static/js/0.e611a833.js
+# https://github.com/wpscott/AcFunDanmaku/blob/master/AcFunDanmu/README.md
+# https://github.com/orzogc/acfundanmu/blob/master/proto.go
+
+

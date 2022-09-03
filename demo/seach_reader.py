a364629eca60cc7c2051b1d37377c844d71b765e
哆啦酱 <dolacmeo@qq.com>
2022-09-03 12:29:06 +0800

    add search demo

---
+++ demo/seach_reader.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+from acfun import Acer
+
+# 实例化一个Acer
+acer = Acer(debug=True)
+
+
+def acfun_search(keyword: str, s_type: [str, None] = None, page: int = 1):
+    print("search:", keyword)
+    obj = acer.AcSearch(keyword, s_type)
+    first_page = obj.page(page)
+    for i, item in enumerate(first_page):
+        print(f"{(i+1):0>2}", item)
+
+
+if __name__ == '__main__':
+    acfun_search("爆蕉一夏")
+    pass
